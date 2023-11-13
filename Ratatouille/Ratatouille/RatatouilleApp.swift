57551c0a0cab3775216c03e3165123dcbb0f685b
Simen <wokiwoki@live.no>
2023-11-13 18:37:08 +0100

    initial commit

---
+++ Ratatouille/Ratatouille/RatatouilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatatouilleApp.swift
+//  Ratatouille
+//
+//  Created by Simen Hansen on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RatatouilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
